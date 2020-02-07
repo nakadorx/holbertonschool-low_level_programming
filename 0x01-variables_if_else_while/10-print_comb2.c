d5962399b317ed3c0408e95dd69f78af2559dc7d
nakadorx <nakadorx@gmail.com>
2020-02-07 23:36:33 +0100

    gergzergrzegrgzerg

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,18 +7,18 @@ int main(void)
 {
 int i, j;
 for (i = 0; i < 10; i++)
-
+{
 for (j = 0; j < 10; j++)
-
+{
 putchar(i + '0');
 putchar(j + '0');
 if (i != 9 || j != 9)
-
+{
 putchar(',');
 putchar(' ');
-
-
-
+}
+}
+}
 putchar('\n');
 return (0);
 }
