c0cfbe0092d90c6bc90fb3504c8ce6eed9ecf8f7
nakadorx <nakador@gmail.com>
2020-02-11 16:03:23 +0100

    frgr

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * times_table - Entry point
+ * Return: Always 0 (success)
+ */
+
+void times_table(void)
+{
+int x, y;
+for (x = 0 ; x <= 9 ; x++)
+{
+for (y = 0 ; y <= 9 ; y++)
+{
+if (x * y < 10)
+{
+_putchar(x * y + '0');
+if (y != 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+}
+else
+{
+_putchar(x * y / 10 + '0');
+_putchar(x * y % 10 + '0');
+if (y != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+_putchar('$');
+_putchar('\n');
+}
+}
