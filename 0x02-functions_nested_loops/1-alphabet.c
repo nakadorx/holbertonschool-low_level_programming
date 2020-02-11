d8ea24908d3869c9755b0ad73f5ce1b2934aa6e6
nakadorx <nakador@gmail.com>
2020-02-11 10:50:34 +0100

    gergergre

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_alphabet - check the code for Holberton School students.
+ *
+ * Return: void.
+ */
+void print_alphabet(void)
+{
+for (char x = 'a'; x <= 'z' ; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+void print_alphabet(void);
+return (0);
+}
