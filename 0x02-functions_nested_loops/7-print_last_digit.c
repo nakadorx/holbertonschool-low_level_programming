aafd8edb3a367f8024266dedfb32dbc0b24ffa23
nakadorx <nakador@gmail.com>
2020-02-11 14:27:21 +0100

    frgr

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,12 @@ int print_last_digit(int n)
 {
 if (n >= 0)
 {
-_putchar(n % 10);
+_putchar(n % 10 + '0');
 return (n % 10);
 }
 else
 {
-_putchar((n * -1) % 10 + '0');
-return ((n * -1) % 10);
+_putchar((n % 10) * -1 + '0');
+return ((n % 10) * -1);
 }
 }
