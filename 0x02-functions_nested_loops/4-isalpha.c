cd60dcb7e943a2a251bfc256f9e1132775c6b688
nakadorx <nakador@gmail.com>
2020-02-11 14:07:26 +0100

    gregr

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _isalpha - check the code for  Holberton School students.
+ * _isalpha - check the code for Holberton School students.
  *  @c : char to check
  * Return: Always 0.
  */
