0e723d07fa28c2d8cafb727b97d40abbed206d4b
nakadorx <nakador@gmail.com>
2020-02-11 14:03:53 +0100

    gregr

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * print_last_digit - check the code for Holberton School students.
+ *  @n : char to check
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+return (n % 10);
+}
