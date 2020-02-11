cf39940ff2b2a5ef77a4d8cd2692e07a8c47b42b
nakadorx <nakador@gmail.com>
2020-02-11 13:54:03 +0100

    gergrerg

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _abs - check the code for Holberton School students.
+ *  @n : char to check
+ * Return: Always 0.
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else
+{
+return (n * (-1));
+}
+}
