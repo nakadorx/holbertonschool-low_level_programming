70f3b8dc9dab9cec454c275c73baad13cf4af323
nakadorx <nakador@gmail.com>
2020-02-11 19:57:03 +0100

    frgr

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,32 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
- * print_last_digit - Entry point
+ * print_to_98 - Entry point
  * @n : is a number
  * Return: Always 0 (success)
  */
 void print_to_98(int n)
 {
-
+int i = n;
+if (n <= 98)
+{
+while (i <= 98)
+{
+if (i == 98)
+printf("%d", i);
+else
+printf("%d, ", i);
+i++;
+}
+}
+else
+{
+while (i >= 98)
+{
+if (i == 98)
+printf("%d", i);
+else
+printf("%d, ", i);
+i--;
+}
+}
 }
