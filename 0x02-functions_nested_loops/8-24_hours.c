f96ff5bcb61fed79404046b492c8346b09bd8660
nakadorx <nakador@gmail.com>
2020-02-11 15:06:10 +0100

    frgr

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,9 +6,9 @@
 void jack_bauer(void)
 {
 int h, m;
-for (h = 0 ; h > 24 ; h++)
+for (h = 0 ; h < 24 ; h++)
 {
-for (m = 0 ; m > 60 ; m++)
+for (m = 0 ; m < 60 ; m++)
 {
 if (h < 10)
 {
@@ -17,7 +17,8 @@ _putchar(h);
 }
 else
 {
-_putchar(h);
+_putchar(h / 10);
+_putchar(h % 10);
 }
 _putchar(':');
 if (m < 10)
@@ -27,7 +28,8 @@ _putchar(m);
 }
 else
 {
-_putchar(m);
+_putchar(m / 10);
+_putchar(m % 10);
 }
 }
 }
