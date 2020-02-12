3f1933bfaa9981052c305d773425483d6ab82972
nakadorx <nakador@gmail.com>
2020-02-12 19:19:01 +0100

    gergr

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+ * print_times_table - Entry point
+ * @n : int
+ * Return: Always 0 (success)
+ */
+
+void print_times_table(int n)
+{
+int x, y;
+if (n > 15 || n < 0)
+{
+_putchar('\0');
+}
+else
+{
+for (x = 0 ; x <= n ; x++)
+{
+for (y = 0 ; y <= n ; y++)
+{
+if (x * y < 10)
+{
+_putchar(x * y + '0');
+if (y != n)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+if (x * (y + 1) < 10)
+{
+_putchar(' ');
+}
+}
+}
+else
+{
+_putchar(x * y / 10 + '0');
+_putchar(x * y % 10 + '0');
+if (y != n)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+}
+}
+_putchar('\n');
+}
+}
+}
