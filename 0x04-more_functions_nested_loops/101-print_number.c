fad83e4b51e10b960c19ab440341c20c2bcedb81
nakadorx <nakadorx@gmail.com>
2020-02-13 22:08:29 +0100

    grgrg

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+* print_number - check the code for Holberton School students.
+* @n : int
+* Return: Always 0.
+*/
+
+void print_number(int n)
+{
+int x = 9, y = 90, v = 1, i, j, z = 1, a;
+if (n < 0)
+{
+n = n * -1;
+_putchar('-');
+}
+while (x < n)
+{
+x = x + y;
+y = y * 10;
+v = v + 1;
+}
+for (j = 1 ; j < v ; j++)
+{
+z = z * 10;
+}
+
+for (i = 0; i  < v; i++)
+{
+a = n / z;
+n = n - (a *z);
+z = z / 10;
+_putchar(a + '0');
+}
+}
