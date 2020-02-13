7bcad2f597d9b2fe664486404a001525c36c459b
nakadorx <nakadorx@gmail.com>
2020-02-13 18:58:26 +0100

    gertgergreg

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int i;
-for (i = 1 ; i <= 100 ; i++)
+for (i = 1 ; i < 100 ; i++)
 {
 if ((i % 3) + (i % 5) == 0)
 {
@@ -27,6 +27,6 @@ else
 printf("%d ", i);
 }
 }
-printf("\n");
+printf("Buzz\n");
 return (0);
 }
