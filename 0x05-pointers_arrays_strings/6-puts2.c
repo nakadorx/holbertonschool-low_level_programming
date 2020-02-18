11a9643f004eeb24ab870e26e09cb452ab96b1d6
nakadorx <nakadorx@gmail.com>
2020-02-18 14:14:12 +0100

    grg

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
 * puts2 - check the code for Holberton School students.
-* @s : char
+* @str : char
 * Return: Always 0.
 */
 void puts2(char *str)
 {
-int i , x = -1;
+int i, x = -1;
 for (i = 0 ; str[i] != '\0' ; i++)
 {
 if (x < 0)
