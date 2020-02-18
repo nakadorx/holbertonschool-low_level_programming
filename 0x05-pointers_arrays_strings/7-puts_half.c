8b85fd8e5d82b838229fe13a26ecbea54aff9e06
nakadorx <nakadorx@gmail.com>
2020-02-18 14:57:36 +0100

    grgrg

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,19 +11,10 @@ while (str[i + 1] != '\0')
 {
 i++;
 }
-if (i % 2 == 0)
-{
 for (x = i / 2 ; str[x] != '\0' ; x++)
 {
 _putchar(str[x]);
 }
-}
-else
-{
-for (x = (i / 2) + 1 ; str[x] != '\0' ; x++)
-{
-_putchar(str[x]);
-}
-}
+
 _putchar('\n');
 }
