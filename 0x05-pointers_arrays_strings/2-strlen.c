770c3a4c088f6839f57139ff0750d5f9e76ce6f7
nakadorx <nakadorx@gmail.com>
2020-02-18 11:24:32 +0100

    vrgr

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* swap_int - check the code for Holberton School students.
+* @s : char
+* Return: Always 0.
+*/
+int _strlen(char *s)
+{
+int i;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
