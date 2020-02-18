39e189d08f4e9ac7645cf2488aa43d322937fbc2
nakadorx <nakadorx@gmail.com>
2020-02-18 16:04:11 +0100

    vgergergt

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* _strcpy - check the code for Holberton School students.
+* @dest : int
+* @src : int
+* Return: Always 0.
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0 ; src[i] != '\0' ; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
