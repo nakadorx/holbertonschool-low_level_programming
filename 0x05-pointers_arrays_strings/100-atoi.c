56db59dd9442c29852e17a6db4b7efccbcca0d6c
nakadorx <nakadorx@gmail.com>
2020-02-18 22:41:35 +0100

    gfrgrg

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,15 +6,15 @@
 */
 int _atoi(char *s)
 {
-    int sgn = 1,i = 1, x = 0;
-    if (*s == '-')
-    {
-        sgn *= -1;
-    }
-    while (*(s+i) > '0' && *(s+i) < 9 && *(s + i) != 0 )
-    {
-        x = (x * 10)  + sgn * (*(s + i) - '0');
-        i++;
-    }
-    return (x);
+int sgn = 1, i = 1, x = 0;
+if (*s == '-')
+{
+sgn *= -1;
+}
+while (*(s + i) > '0' && *(s + i) < 9 && *(s + i) != 0)
+{
+x = (x * 10)  + sgn * (*(s + i) - '0');
+i++;
+}
+return (x);
 }
