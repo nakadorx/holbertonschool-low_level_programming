a6b6aaf22bc4667c50abaffdca480088d5b2bffb
nakadorx <nakadorx@gmail.com>
2020-02-18 14:41:42 +0100

    gvrgrg

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,28 +6,24 @@
 */
 void puts_half(char *str)
 {
-int x, i = 0 ;
+int x, i = 0;
 while (str[i + 1] != '\0')
 {
 i++;
 }
-if ( i % 2 == 0)
+if (i % 2 == 0)
 {
-
-    for (x = i / 2 ; str[x] != '\0' ; x++ )
-    {
-        _putchar(str[x]);
-    }
-
+for (x = i / 2 ; str[x] != '\0' ; x++)
+{
+_putchar(str[x]);
+}
 }
 else
 {
-    for (x = (i / 2) + 1 ; str[x] != '\0' ; x++ )
-    {
-        _putchar(str[x]);
-    }
+for (x = (i / 2) + 1 ; str[x] != '\0' ; x++)
+{
+_putchar(str[x]);
 }
-
-
+}
 _putchar('\n');
 }
