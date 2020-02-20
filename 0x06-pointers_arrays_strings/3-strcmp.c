685d85243b6793da00a86d0c7af0082017d55fca
nakadorx <nakadorx@gmail.com>
2020-02-20 16:35:22 +0100

    grgr

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,7 +7,8 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 != '\0' && *s2 != '\0' && s1[i] == s2[i])
+int i = 0;
+while (*(s1 + i + 1) != '\0' && *(s2 + i + 1) != '\0' && s1[i] == s2[i])
 {
 i++;
 }
