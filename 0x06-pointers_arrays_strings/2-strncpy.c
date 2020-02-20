2616c1986c98f71838f5883e56c9c1746acda3f0
nakadorx <nakadorx@gmail.com>
2020-02-20 15:50:48 +0100

    grgr

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* *_strncat - check the code for Holberton School students.
+* @dest : int
+* @src : int
+* @n : int
+* Return: Always 0.
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+while (dest[i] != '\0')
+{
+dest[i] = '\0'; 
+i++;
+}
+i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
