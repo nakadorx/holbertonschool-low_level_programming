a837b2b8ff9f2cf13c6b5896358b1fefc3215ebd
nakadorx <nakadorx@gmail.com>
2020-02-20 14:31:48 +0100

    gbrh

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,17 +8,17 @@
 */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0, x = 0;
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
-    while (src[x] != '\0')
-    {
-        dest[i] = src[x];
-        x++;
-        i++;
-    }
-    dest[i] = '\0';
-    return(dest);
+int i = 0, x = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[x] != '\0')
+{
+dest[i] = src[x];
+x++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
 }
