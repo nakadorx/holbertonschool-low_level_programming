8072de395a1ac716ea251d27f90da689b34cdc9f
nakadorx <nakadorx@gmail.com>
2020-02-20 16:52:55 +0100

    frgrg

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* rev_string - check the code for Holberton School students.
+* @a : char
+* @n : char
+* Return: Always 0.
+*/
+
+void reverse_array(int *a, int n)
+{
+int  x;
+int m[509];
+for (x = n  ; x >= 0  ; x--)
+{
+a[n - x] = m[x];
+}
+}
