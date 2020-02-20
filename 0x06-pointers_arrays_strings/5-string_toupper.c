b9938248658d390619f0bd2baa03bc9721721aaf
nakadorx <nakadorx@gmail.com>
2020-02-20 17:52:03 +0100

    gregregreg

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,9 +7,9 @@
 char *string_toupper(char *x)
 {
 int i = 0;
-while (x[i - 1] != '\0')
+while (x[i] != '\0')
 {
-if (!(x[i] >= 'A' && x[i] <= 'Z'))
+if (x[i] >= 'a' && x[i] <= 'z')
 {
 x[i] = x[i] - 32;
 }
