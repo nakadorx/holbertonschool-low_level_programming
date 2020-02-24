13f564ebae61d8b96558e45fc538fbcb64bea00b
nakadorx <nakadorx@gmail.com>
2020-02-24 21:43:58 +0100

    grtgr

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_chessboard - check the code for Holberton School students.
+ * @a: int
+ *
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+    int i = 0, j = 0;
+   while (i < 8)
+   {
+       while (j < 8)
+       {
+           _putchar(a[i][j]); 
+           j++;
+       }
+       j= 0;
+       i++;
+        _putchar('\n');
+        
+   }
+}
