c94e196f276d00b25fe1ebd64179a68a19ee937f
nakadorx <nakadorx@gmail.com>
2020-02-24 21:29:50 +0100

    grgrg

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,35 +11,27 @@ char *_strstr(char *haystack, char *needle)
 int i = 0, j = 0;
 while (haystack[i] != '\0')
 {
-    while(needle[j] != '\0')
-    {
-        if (haystack[i] == needle[0])
-        {
-            if (haystack[i + j] == needle[j])
-            {
-                j++;
-            }
-            else
-            {
-                break;
-            }
-        }
+while (needle[j] != '\0' && (haystack[i] == needle[0]))
+{
+if (haystack[i + j] == needle[j])
+{
+j++;
+}
+else
+{
+break;
+}
+}
 
-    }
-        if (needle[j] != '\0')
-        {
-
-        j = 0;
-        i++;
-        }
-        else
-        {
-        return (haystack + i);
-        }
-
-    
+if (needle[j] != '\0')
+{
+j = 0;
+i++;
+}
+else
+{
+return (haystack + i);
+}
 }
 return (0);
 }
-
-
