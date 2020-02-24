1835b0400f913bd837759bc56811d3d9225a1043
nakadorx <nakadorx@gmail.com>
2020-02-24 23:10:19 +0100

    grgrg

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* set_string - check the code for Holberton School students.
+* @s: int
+* @to : int
+* Return: Always 0.
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
