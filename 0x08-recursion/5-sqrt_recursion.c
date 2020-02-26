44c431b8b11eda76b8b403d054e94df81e87e8ff
nakadorx <nakadorx@gmail.com>
2020-02-26 19:03:30 +0100

    ggrfgr

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * sx - check the code for Holberton School students.
+ * @i: int
+ * @n: int
+ * Return: Always 0.
+ */
+
+int sx(int i, int n)
+{
+if (i * i == n)
+return (i);
+if (i * i > x)
+return (-1);
+return (i + 1, n);
+}
+/**
+ * _sqrt_recursion - check the code for Holberton School students.
+ * @n: int
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int n)
+{
+if (n == 0)
+return (0);
+return (sx(1, n));
+}
