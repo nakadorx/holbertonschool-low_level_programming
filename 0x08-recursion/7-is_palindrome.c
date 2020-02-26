785deb69ca9e022b486d57cc0308f40d667e0310
nakadorx <nakadorx@gmail.com>
2020-02-26 19:56:25 +0100

    grrgr

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,45 @@
+
+#include "holberton.h"
+/**
+ * sl - check the code for Holberton School students.
+ * @s: int
+ * Return: Always 0.
+ */
+
+int sl(char *s)
+{
+if (!*s)
+return (0);
+else
+return (1 + sl(s + 1));
+}
+
+/**
+ * pn - check the code for Holberton School students.
+ * @i: int
+ * @n: int
+ * Return: Always 0.
+ */
+
+int pl(int l, int i, char *s)
+{
+if (i > l / 2)
+return (1);
+else if (s[i] != s[l - i - 1])
+return (0);
+else
+return (pl(l, i + 1, s));
+}
+
+/**
+ * pn - check the code for Holberton School students.
+ * @i: int
+ * @n: int
+ * Return: Always 0.
+ */
+int is_palindrome(char *s)
+{
+int l;
+l = sl(s);
+return (pl(l, 0, s));
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -11,5 +11,6 @@ int sx(int i, int n);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int pn(int i, int n);
+int is_palindrome(char *s);
 
 #endif /* _HOLBERTON_H_ */
