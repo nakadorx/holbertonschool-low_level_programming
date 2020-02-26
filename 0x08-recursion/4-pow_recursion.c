6a4425650f55c5c7c955c51e1fc64529caff541a
nakadorx <nakadorx@gmail.com>
2020-02-26 18:35:06 +0100

    fggrg

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - check the code for Holberton School students.
+ * @x: int
+ * @y: int
+ * Return: Always 0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+    return(-1);
+    else if (y == 0)
+    return (1);
+    return (x * _pow_recursion(x, y - 1));
+}
