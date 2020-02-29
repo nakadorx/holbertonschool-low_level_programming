79a30f00c1f4c01ee63b658c83d61120dced1ddf
nakadorx <nakadorx@gmail.com>
2020-02-29 15:00:45 +0100

    grgrg

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,11 +5,12 @@
  * @argv : int
  * Return: Always 0.
  */
-int main(int argc, char const *argv[])
+int main(int argc, char  *argv[])
 {
 int i;
 for (i = 0; i < argc ; i++)
 {
 printf("%s\n", argv[i]);
 }
+return (0);
 }
