95409cece3c239222a391484ba9b6ce16a883e28
nakadorx <nakadorx@gmail.com>
2020-02-29 14:05:08 +0100

    grgrg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: int
+ * @argv : int
+ * Return: Always 0.
+ */
+int main(int argc[], char const *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
