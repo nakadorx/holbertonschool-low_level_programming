a8125f63b8ff1401b6ef5c68fe1bd1950df7e3c8
nakadorx <nakadorx@gmail.com>
2020-02-29 16:23:40 +0100

    gge

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code for Holberton School students.
+ * @argc: int
+ * @argv : int
+ * Return: Always 0.
+ */
+int main(int argc, char  *argv[])
+{
+int x, i, s = 0;
+int c[5] = {25, 10, 5, 2, 1};
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+if ( atoi(argv[1])<= 0)
+{
+puts("0");
+return (1);
+}
+x = atoi(argv[1]);
+while (x != 0)
+{
+for (i = 0; i < 5 ; i++)
+{
+    if (x >= c[i])
+    {
+        x -= c[i];
+        s++;
+        break;
+    }
+}
+}
+printf("%d\n", s);
+return (0);
+}
+
