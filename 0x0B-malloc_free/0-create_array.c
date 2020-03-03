8ea802bf211522bc0817cf6f915b778f88945fd1
nakadorx <nakadorx@gmail.com>
2020-03-03 18:05:00 +0100

    grgrg

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * create_array - check the code for Holberton School students.
+ * @c: int
+ * @size: int
+ * Return: Always 0.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+unsigned int i;
+char *t;
+
+if (size == 0)
+return (NULL);
+t = malloc(size * sizeof(char));
+for (i = 0; i < size; i++)
+t[i] = c;
+return (t);
+}
