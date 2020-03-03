13a1f1f8d94d8eebebc8ab47ca4412ff00de8df1
nakadorx <nakadorx@gmail.com>
2020-03-03 19:57:07 +0100

    grgrg

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _strdup - check the code for Holberton School students.
+ * @str: int
+ * Return: Always 0.
+ */
+char *str_concat(char *s1, char *s2)
+{
+unsigned int i, j, m;
+char *x;
+if (s1 == NULL)
+i = 0;
+else
+{
+while (s1[i])
+i++;
+}
+if (s2 == NULL)
+j = 0;
+else
+{
+while (s2[j])
+j++;
+}
+x = malloc((i + j + 1) *sizeof(char));
+if (x == NULL)
+return (NULL);
+for (m = 0; m < i; m++)
+x[m] = s1[m];
+for (m = 0; m < j; m++)
+x[m + i] = s2[m];
+x[i + j] = '\0';
+return (x);
+}
