94354517e12ba951f42b8aeb2ab6aee93512d857
nakadorx <nakadorx@gmail.com>
2020-03-04 22:46:02 +0100

    grgrg

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,10 +12,7 @@ void free_grid(int **grid, int height)
 int i = 0;
 if (grid == NULL || height <= 0)
 return;
-while (i < height)
-{
+for  (;i < height ; i++)
 free(grid[i]);
-i++;
-}
 free(grid);
 }
