26fddc5c5d3a024e01dd8d90833ec9ffad6500f0
nakadorx <nakadorx@gmail.com>
2020-03-04 19:30:46 +0100

    ggrgrg

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * free_grid - check the code for Holberton School students.
+ * @grid: int
+ * @height: int
+ * Return: Always 0.
+ */
+void free_grid(int **grid, int height)
+{
+int i = 0;
+if (grid  == NULL || height <= 0)
+return (NULL);
+while (i < height)
+{
+free(grid[i]);
+i++;
+}
+free(grid;)
+}
