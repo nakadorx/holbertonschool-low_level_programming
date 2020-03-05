486f6cca62a8a0351245443e80170ab0d0519492
nakadorx <nakadorx@gmail.com>
2020-03-05 14:16:16 +0100

    grgrg

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * malloc_checked - check the code for Holberton School students.
+ * @b: int
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p;
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
+}
