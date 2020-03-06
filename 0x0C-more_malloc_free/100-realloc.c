a4beb8c14a110d114bd7c96b7eb52c6ddc3b73ee
nakadorx <nakadorx@gmail.com>
2020-03-06 19:58:39 +0100

    grgrg

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _realloc - check the code for Holberton School students.
+ * @ptr: int
+ * @old_size: int
+ * @new_size: int
+ * Return: Always 0.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+if (ptr == NULL)
+{
+free(ptr);
+ptr = malloc(new_size);
+return (ptr);
+}
+else if (new_size == 0)
+{
+return (ptr);
+}
+else if (new_size == old_size)
+return (ptr);
+if (new_size > old_size)
+return (ptr); 
+else
+ptr = malloc(new_size);
+free(ptr);
+return (ptr);  
+}
