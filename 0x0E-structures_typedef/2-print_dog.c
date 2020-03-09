da4a5625f6f95903993e45c4f8ce94893c871c8b
nakadorx <nakadorx@gmail.com>
2020-03-09 22:04:26 +0100

    grgr

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,10 +1,7 @@
  #include "dog.h"
 /**
- * init_dog - check the code for Holberton School students.
+ * print_dog - check the code for Holberton School students.
  * @d: int
- * @name : int
- * @age : int
- * @owner : int
  * Return: Always 0.
  */
 void print_dog(struct dog *d)
@@ -15,6 +12,5 @@ if (d->owner == NULL)
 d->owner = "(nil)";
 if (d->name == NULL)
 d->name = "(nil)";
-
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
