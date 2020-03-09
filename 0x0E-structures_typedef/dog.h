123f3842b735f75edb4f69370b8bbc076936ea79
nakadorx <nakadorx@gmail.com>
2020-03-09 19:21:15 +0100

    bgehreg

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef _0_DOG_H_
 #define _0_DOG_H_
-
+/**
+ * struct dog - check the code for Holberton School students.
+ * @name: int
+ * @age : int
+ * @owner : int
+ * Return: Always 0.
+ */
 typedef struct dog
 {
 char *name;
