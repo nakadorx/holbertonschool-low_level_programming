f701c551907ffb75f49bf06fbb86376ba8c50c1d
nakadorx <nakadorx@gmail.com>
2020-03-14 19:27:06 +0100

    1-task baliz

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,20 +5,19 @@
  * print_numbers - check the code for Holberton School students.
  * @separator : int
  * @n: int
- * 
  * Return: Always 0.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list params;
-unsigned int i;
-
+unsigned int i = 0;
 va_start(params, n);
-for (i = 0; i < n ; i++)
+while (i < n)
 {
 printf("%d", va_arg(params, int));
 if (separator && i < n - 1)
 printf("%s", separator);
+i++;
 }
 printf("\n");
 va_end(params);
