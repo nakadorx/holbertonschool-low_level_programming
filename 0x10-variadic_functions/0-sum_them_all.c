3ad692afaeb3dc9c013bce1dc003606eb9a46205
nakadorx <nakadorx@gmail.com>
2020-03-14 18:50:04 +0100

    0-sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,13 +7,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    if (n == 0)
-    return (0);
-    unsigned int i = 0;
-    int sum = 0;
-    va_list params;
-    va_start(params, n);
-    for (;i < n; i++)
-    sum += va_arg(params,int);
-    return (sum);
+if (n == 0)
+return (0);
+unsigned int i = 0;
+int sum = 0;
+va_list params;
+va_start(params, n);
+for (; i < n; i++)
+sum += va_arg(params, int);
+return (sum);
 }
