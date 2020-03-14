77e7666fe9e2022d6df291b0e52ace018f945204
nakadorx <nakadorx@gmail.com>
2020-03-14 19:09:47 +0100

    i5dim

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,10 +7,11 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-if (n == 0)
-return (0);
 unsigned int i = 0;
 int sum = 0;
+
+if (n == 0)
+return (0);
 va_list params;
 va_start(params, n);
 for (; i < n; i++)
