206b0bc8a635f0e5f8bd18999955d1799d2ea082
nakadorx <nakadorx@gmail.com>
2020-03-14 18:47:14 +0100

    grgrg

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - check the code for Holberton School students.
+ * @n: int
+ * Return: Always 0.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+    if (n == 0)
+    return (0);
+    unsigned int i = 0;
+    int sum = 0;
+    va_list params;
+    va_start(params, n);
+    for (;i < n; i++)
+    sum += va_arg(params,int);
+    return (sum);
+}
