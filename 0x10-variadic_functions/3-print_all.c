d29b1596d5d6686c5ba2b8cfdb1424c690e4618b
nakadorx <nakadorx@gmail.com>
2020-03-14 20:42:21 +0100

    grgrg

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,7 +4,6 @@
 /**
  * print_all - check the code for Holberton School students.
  * @format : int
- * 
  * Return: Always 0.
  */
 
@@ -14,38 +13,37 @@ va_list params;
 unsigned int i = 0;
 char *separator = ", ";
 char *ch;
+
 va_start(params, format);
-i = 0;
 while (format && format[i])
 {
 switch (format[i])
 {
-    case  's' :
-        ch = va_arg(params, char *);
-            if (ch == NULL)
-                ch = "(nil)";
-                printf("%s", ch);
-                if (format[i + 1])
-                printf("%s", separator);
-        break;
-    case  'i' :
-        printf("%d", va_arg(params, int));
-        if (format[i + 1])
-        printf("%s", separator);
-        break;
-    case 'f':
-        printf("%f", va_arg(params, double));
-        if (format[i + 1])
-        printf("%s", separator);
-    case 'c':
-        printf("%c", va_arg(params, int));
-        if (format[i + 1])
-        printf("%s", separator);
-        break;
-    default:
-        break;
+case  's':
+ch = va_arg(params, char *);
+if (ch == NULL)
+ch = "(nil)";
+printf("%s", ch);
+if (format[i + 1])
+printf("%s", separator);
+break;
+case  'i':
+printf("%d", va_arg(params, int));
+if (format[i + 1])
+printf("%s", separator);
+break;
+case 'f':
+printf("%f", va_arg(params, double));
+if (format[i + 1])
+printf("%s", separator);
+case 'c':
+printf("%c", va_arg(params, int));
+if (format[i + 1])
+printf("%s", separator);
+break;
+default:
+break;
 }
-
 i++;
 }
 va_end(params);
