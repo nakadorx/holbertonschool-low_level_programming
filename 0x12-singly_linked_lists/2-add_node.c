b8cb79a84ad6a96a0afc4673c1224406de010144
nakadorx <nakadorx@gmail.com>
2020-03-21 17:28:57 +0100

    task 2 iguess done

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_node - check the code for Holberton School students.
+ * @h : int
+ * @str : int
+ * Return: Always 0.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
+list_t *new;
+unsigned int i = 0;
+
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
+new->str = strdup(str);
+while (str[i])
+i++;
+new->len = i;
+new->next = *head;
+*head = new;
+return (*head);
+}
