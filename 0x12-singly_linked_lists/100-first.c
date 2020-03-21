da82009af12691d393d9b9c595157f0ffbc4827d
nakadorx <nakadorx@gmail.com>
2020-03-21 17:51:52 +0100

    x

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * x - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+
+void x(void)
+{
+printf("You're beat! and yet, you must allow,");
+printf("\nI bore my house upon my back!\n");
+}
