a3b713ade3dbcd5ec17efd540e68d7cd10bac343
nakadorx <nakadorx@gmail.com>
2020-03-24 14:54:02 +0100

    10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - check the code for Holberton School students.
+ * @head : int
+ * @index : int
+ * Return: Always 0.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *x = *head, *next;
+
+	if (index != 0)
+	{
+		while (i < index - 1 && x != NULL)
+		{
+			x = x->next;
+			i++;
+			if (x == NULL || x->next == NULL)
+				return (-1);
+		}
+	}
+	next = x->next;
+	if (index != 0)
+	{
+		x->next = next->next;
+		free(next);
+	}
+	else
+	{
+		free(x);
+		*head = next;
+	}
+	return (1);
+}
