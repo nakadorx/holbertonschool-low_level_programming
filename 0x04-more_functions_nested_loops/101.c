fcca64dbce1276d559a04f51c573071929d02734
nakadorx <nakadorx@gmail.com>
2020-03-24 14:59:32 +0100

    fixs

--- 0x04-more_functions_nested_loops/101.c
+++ 0x04-more_functions_nested_loops/101.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <limits.h>
 /**
  * main - check the code for Holberton School students.
  *
@@ -16,6 +16,8 @@ int main(void)
     print_number(0);
     _putchar('\n');
     print_number(-98);
+    _putchar('\n');
+    print_number(INT_MAX);
     _putchar('\n');
     return (0);
 }
