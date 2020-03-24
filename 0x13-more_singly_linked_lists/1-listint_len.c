a1e50aad5c220058c9b88e11f399a97067982eaa
nakadorx <nakadorx@gmail.com>
2020-03-24 11:44:02 +0100

    1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+
+#include "lists.h"
+/**
+ * listint_len - check the code for Holberton School students.
+ * @h : int
+ * Return: Always 0.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+size_t i = 0;
+
+while (h != NULL)
+{
+h = h->next;
+i++;
+}
+return (i);
+}
