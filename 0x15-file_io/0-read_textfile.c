d1c80a6164177b0d28f3565116b2117b112225b3
nakadorx <nakadorx@gmail.com>
2020-03-31 16:32:44 +0200

    0 fix betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,21 +9,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	int file;
-	ssize_t rf,wf;
+	ssize_t rf, wf;
 
 	if (!filename)
 		return (0);
 	file = open(filename, O_RDONLY);
 	if (file == -1)
-    	return (0);
+		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
-	rf = read(file,buf,letters);
-	wf = write(STDOUT_FILENO,buf,rf);
+	rf = read(file, buf, letters);
+	wf = write(STDOUT_FILENO, buf, rf);
 	if (wf == -1)
 		return (0);
 
 	close(file);
-	return(wf);
+	return (wf);
 }
