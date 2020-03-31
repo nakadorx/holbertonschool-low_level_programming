3eccb2327115a98db6b0ecafb792754d5d9348dd
nakadorx <nakadorx@gmail.com>
2020-03-31 17:01:40 +0200

    test 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	while (text_content[i])
 		i++;
+	if (!text_content)
+		text_content = "";
 	wf = write(file, text_content, i);
 	if (wf == -1)
 		return (-1);
