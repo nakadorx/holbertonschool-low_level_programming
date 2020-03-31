c99bd3de88f3d841913ec8a9c213cb96c72df62f
nakadorx <nakadorx@gmail.com>
2020-03-31 17:42:35 +0200

    2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * create_file - check the code for Holberton School students.
+ * @text_content : int
+ * @filename : int
+ * Return: Always 0.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, i = 0;
+	ssize_t wf;
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_WRONLY | O_APPEND 0600);
+	if (file == -1)
+		return (-1);
+	while (text_content[i])
+		i++;
+	wf = write(file, text_content, i);
+	if (wf == -1)
+		return (-1);
+	close(file);
+	return (1);
+}
