3132324060b22c06e54259231bc0300a427fa77e
nakadorx <nakadorx@gmail.com>
2020-03-31 17:03:08 +0200

    brasomik

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,8 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	while (text_content[i])
 		i++;
-	if (!text_content)
-		text_content = "";
+	i--;
 	wf = write(file, text_content, i);
 	if (wf == -1)
 		return (-1);
