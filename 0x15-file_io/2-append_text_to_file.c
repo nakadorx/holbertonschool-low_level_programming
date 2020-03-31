34cc04283fc31ac6459fa31b9fc7ef56d7351722
nakadorx <nakadorx@gmail.com>
2020-03-31 17:46:24 +0200

    9 plz green

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,9 +16,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	file = open(filename, O_WRONLY | O_APPEND, 0600);
 	if (file == -1)
 		return (-1);
-	while (text_content[i])
-		i++;
-	wf = write(file, text_content, i);
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+		wf = write(file, text_content, i);
+	}
 	if (wf == -1)
 		return (-1);
 	close(file);
