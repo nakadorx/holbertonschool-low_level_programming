82583603155de6c2cb8f01dc30f599b16ab33bc9
Nakadorx <nakadorx@gmail.com>
2020-05-04 19:11:50 +0200

    grgr

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,25 @@
+
+#include "holberton.h"
+/**
+* *_strncat - check the code for Holberton School students.
+* @dest : int
+* @src : int
+* @n : int
+* Return: Always 0.
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, x = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (x < n)
+{
+dest[i] = src[x];
+x++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
